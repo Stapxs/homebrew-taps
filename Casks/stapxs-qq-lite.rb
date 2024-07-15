d1bc6429c9c612c88869027a57b8f3628740c038
林小槐 <1007028430.stapx@gmail.com>
2024-07-15 16:18:40 +0800

    Update stapxs-qq-lite.rb

--- Casks/stapxs-qq-lite.rb
+++ Casks/stapxs-qq-lite.rb
@@ -1,9 +1,9 @@
 cask "stapxs-qq-lite" do
     arch arm: "arm64", intel: "x64"
   
-    version "2.7.13"
-    sha256 arm:   "d0b9d9d3c985ff6f5917e4f5822cb71aecd818f29de54d44cc662fe072c64874",
-           intel: "963c657cbd3ff22e07693bc2bfea454a40a9f91d9a33fa9d3cce42255d328ffe"
+    version "2.7.15"
+    sha256 arm:   "c9e233cfa9e2f44b1eb61dc9bd236730627b37a18e12f61d4ee0c1387986ae56",
+           intel: "2ae3b1e4f898c41a40e1303a4d304109008442a25c69a9797906964a21bbbc40"
   
     url "https://github.com/Stapxs/Stapxs-QQ-Lite-2.0/releases/download/v#{version}/Stapxs.QQ.Lite-#{version}-#{arch}.dmg"
     name "Stapxs QQ Lite"
