0aa226a4809dec80f36864221b9b7f1017467819
林小槐 <1007028430.stapx@gmail.com>
2024-09-30 09:51:12 +0800

    Update stapxs-qq-lite.rb

--- Casks/stapxs-qq-lite.rb
+++ Casks/stapxs-qq-lite.rb
@@ -1,7 +1,7 @@
 cask "stapxs-qq-lite" do
     arch arm: "arm64", intel: "x64"
   
-    version "2.8.6"
+    version "2.9.0"
     sha256 arm:   "fd5b2685c7180628271c6b1a61c7300f7a5b82324a0d7af7b1af8a8673f6659b",
            intel: "a931b7e96bdb5493c7e17adf6157d1bfe5f7b65594feb968992b31e13c9f9d57"
   
