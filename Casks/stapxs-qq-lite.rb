c49a28c391ee4ba21e922f3b6ba161bd2abe55ca
林小槐 <1007028430.stapx@gmail.com>
2025-06-03 16:00:47 +0800

    Update stapxs-qq-lite.rb

--- Casks/stapxs-qq-lite.rb
+++ Casks/stapxs-qq-lite.rb
@@ -1,13 +1,13 @@
 cask "stapxs-qq-lite" do
-    arch arm: "arm64", intel: "x64"
+    arch arm: "aarch64", intel: "x64"
   
-    version "3.1.4"
-    sha256 arm:   "ecbe380ed0a3a94f92ff6784f7ca3105b58cc55e51c46cd97b3a1f30739b3c61",
-           intel: "7f3d82f479a7a4794db7aa61a9a3ec6b88e32bd964ca574460493fe89474e3d6"
+    version "3.2.0"
+    sha256 arm:   "a4fb2debbca06a107eb39af997dd2b309613e8f5eee84c6c4de1e93b0b7309b6",
+           intel: "8787ea28d676e4235a62b504c26cb1e3b57199b5fd6e92e2917191ea4e8f4032"
   
     url "https://github.com/Stapxs/Stapxs-QQ-Lite-2.0/releases/download/v#{version}/Stapxs.QQ.Lite-#{version}-#{arch}.dmg"
     name "Stapxs QQ Lite"
-    desc "一个兼容 OneBot 协议的非官方网页版 QQ 客户端。"
+    desc "一个兼容 OneBot 协议的非官方 QQ Web 客户端、全平台客户端实现！"
     homepage "https://stapxs.github.io/Stapxs-QQ-Lite-2.0"
   
     livecheck do
